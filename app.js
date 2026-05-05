f55e0c446ba725d25237fd7e02522ee2cae61dbc
ruthu <rutuarakere@gmail.com>
2026-05-05 15:28:36 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end("Hello from AWS EC2 🚀");
+  res.end("Hello from me node js and aws");
 }).listen(3000, () => {
   console.log("Server running on port 3000");
 });
